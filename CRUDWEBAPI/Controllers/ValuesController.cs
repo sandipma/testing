b614a3da1633df3bde34dcb931cef2f8cdfd8682
sandip <sandipmane200@gmail.com>
2023-07-29 00:27:12 +0530

    sdfdsfdsfsfsd

--- CRUDWEBAPI/Controllers/ValuesController.cs
+++ CRUDWEBAPI/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace CRUDWEBAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2" ,"values"};
         }
 
         // GET api/values/5
